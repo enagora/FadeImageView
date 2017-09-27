a648340c0d8c47de561daca686642c55d4d813ba
Uraitz Olaizola <uolaizola@enagora.com>
2017-09-27 23:10:59 +0200

    subida de proyecto completo

---
+++ FadeImageView.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+using Android.Content;
+using Android.Util;
+using Android.Widget;
+using Android.Graphics;
+using Android.Views.Animations;
+using Android.Graphics.Drawables;
+
+namespace FadeImageSample
+{
+    public class FadeImageView : ImageView
+    {
+        Animation fadeInAnimation;
+        Animation fadeOutAnimation;
+
+        public FadeImageView(Context ctx) : base(ctx)
+        {
+            Initialize();
+        }
+
+        public FadeImageView(Context context, IAttributeSet attrs) :
+            base(context, attrs)
+        {
+            Initialize();
+        }
+
+        public FadeImageView(Context context, IAttributeSet attrs, int defStyle) :
+            base(context, attrs, defStyle)
+        {
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            fadeInAnimation = new AlphaAnimation(0, 1)
+            {
+                Duration = 500
+            };
+            fadeOutAnimation = new AlphaAnimation(1, 0)
+            {
+                Duration = 100
+            };
+        }
+
+
+        void DoAnimation(bool really, Action changePic)
+        {
+            if (!really)
+                changePic();
+            else
+            {
+                _changePicAnimation = changePic;
+                fadeOutAnimation.AnimationEnd += FadeOutAnimation_AnimationEnd; ;
+                StartAnimation(fadeOutAnimation);
+            }
+        }
+        private Action _changePicAnimation;
+        private void FadeOutAnimation_AnimationEnd(object sender, Animation.AnimationEndEventArgs e)
+        {
+            _changePicAnimation();
+            StartAnimation(fadeInAnimation);
+            fadeOutAnimation.AnimationEnd -= FadeOutAnimation_AnimationEnd;
+        }
+
+        public void SetImageBitmap(Bitmap bm, bool animate)
+        {
+            DoAnimation(animate, () => SetImageBitmap(bm));
+        }
+
+        public void SetImageDrawable(Drawable drawable, bool animate)
+        {
+            DoAnimation(animate, () => SetImageDrawable(drawable));
+        }
+
+        public void SetImageResource(int resId, bool animate)
+        {
+            DoAnimation(animate, () => SetImageResource(resId));
+        }
+
+        public void SetImageURI(Android.Net.Uri uri, bool animate)
+        {
+            DoAnimation(animate, () => SetImageURI(uri));
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace FadeImageView
+{
+    [Activity(Label = "FadeImageView", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private FadeImageSample.FadeImageView image;
+        private Button button;
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            image = FindViewById<FadeImageSample.FadeImageView>(Resource.Id.imageView1);
+            button = FindViewById<Button>(Resource.Id.btnChange);
+
+            button.Click += BtnChange_Click;
+        }
+
+        private void BtnChange_Click(object sender, System.EventArgs e)
+        {
+
+            var catIDs =new int[] { Resource.Drawable.cat1, Resource.Drawable.cat2, Resource.Drawable.cat3 };
+
+            Random rnd = new Random();
+            int currentCat = rnd.Next(0, catIDs.Length);
+
+            image.SetImageResource(catIDs[currentCat],true);
+        }
+    }
+}
+
